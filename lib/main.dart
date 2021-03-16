4a1f95e37118b41932a217bda4c750ca55c4cfe2
Safwan <safwan9f@gmail.com>
2021-03-16 18:03:01 +0300

    Implement providers and listening to them

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shopy/screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import 'services/product_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +12,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shopy',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'Shopy',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (_) => ProductsOverviewScreen(),
+          ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (_) => ProductsOverviewScreen(),
-        ProductDetailScreen.routeName: (_) => ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../services/product_service.dart';
+import '../widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ProductItem(
+          product: products[index],
+        );
+      },
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
