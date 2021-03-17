5eabbccf2a9a4beaf2f81c579e15e9076a6cdf5e
Safwan <safwan9f@gmail.com>
2021-03-18 02:26:32 +0300

    Create OrderItem widget and style it

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopy/services/orders_service.dart';
+import 'package:shopy/widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final orders = Provider.of<Orders>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orders.orders.length,
+        itemBuilder: (context, index) => OrderItem(
+          order: orders.orders[index],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shopy/models/order.dart';
+import 'package:intl/intl.dart';
+
+class OrderItem extends StatelessWidget {
+  final Order order;
+
+  OrderItem({this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
